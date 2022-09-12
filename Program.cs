45e16c47a12679def3a9ade0d723981e0441350c
MatejPemper <90246393+MatejPemper@users.noreply.github.com>
2022-09-12 12:19:59 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanje2._1
+{
+    internal class Program
+    {
+        private static string abc;
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Unesite rečenicu: ");
+
+             abc= Console.ReadLine();
+
+           string abc1;
+           abc1 =  abc.Replace(" ", "_");
+            Console.WriteLine(abc1);
+
+            Console.ReadKey();
+
+        }
+    }
+}
